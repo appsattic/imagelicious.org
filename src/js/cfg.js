a5ce4f42425ff2d4ace468dc61a605665ae41842
Andrew Chilton <andychilton@gmail.com>
2016-07-23 22:44:35 +1200

    Add the src/js/cfg.js

---
+++ src/js/cfg.js
@@ -0,0 +1,16 @@
+// --------------------------------------------------------------------------------------------------------------------
+
+"use strict"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+module.exports = {
+  firebase : {
+    apiKey        : process.env.FIREBASE_API_KEY,
+    authDomain    : process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL   : process.env.FIREBASE_DATABASE_URL,
+    storageBucket : process.env.FIREBASE_STORAGE_BUCKET,
+  },
+}
+
+// --------------------------------------------------------------------------------------------------------------------
